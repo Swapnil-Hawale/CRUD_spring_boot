83d976084ccad980bf42c32eceb2aca8684c664d
Swapnil <swapnilhawale8399@gmail.com>
2024-01-05 13:07:35 +0530

    My Third Commit

---
+++ src/main/java/com/product/controller/HomeController.java
@@ -0,0 +1,80 @@
+package com.product.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.product.entity.Products;
+import com.product.repository.ProductRepository;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private ProductRepository productRepo;
+
+	@GetMapping("/")
+	public String home(Model m) {
+
+		List<Products> list = productRepo.findAll();
+		m.addAttribute("all_products", list);
+
+		return "index";
+	}
+
+
+	@GetMapping("/load_form")
+	public String loadForm() {
+		return "add";
+	}
+
+	@GetMapping("/edit_form/{id}")
+	public String editForm(@PathVariable(value = "id") long id, Model m) {
+
+		Optional<Products> product = productRepo.findById(id);
+
+		Products pro = product.get();
+		m.addAttribute("product", pro);
+
+		return "edit";
+	}
+
+	@PostMapping("/save_products")
+	public String saveProducts(@ModelAttribute Products products, HttpSession session) {
+
+		productRepo.save(products);
+		session.setAttribute("msg", "Product Added Sucessfully..");
+
+		return "redirect:/load_form";
+	}
+
+	@PostMapping("/update_products")
+	public String updateProducts(@ModelAttribute Products products, HttpSession session) {
+
+		productRepo.save(products);
+		session.setAttribute("msg", "Product Update Sucessfully..");
+
+		return "redirect:/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteProducts(@PathVariable(value = "id") long id, HttpSession session) {
+		productRepo.deleteById(id);
+		session.setAttribute("msg", "Product Delete Sucessfully..");
+
+		return "redirect:/";
+
+	}
+
+}
